094f41390b07c2e4417db46733a995552587cc97
Ana Zweni <anamandla38@gmail.com>
2025-06-26 06:38:43 +0000

    how can I publish this website on github pages?

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,29 @@
 import type {NextConfig} from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true, // Necessary for static export
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
